f8558ccbdfa72f0c3892879908926f4484762898
shankar <shankar@shankars-mac-mini-2.local>
2018-04-13 11:15:47 +0530

    added fastlane

--- JenkinsTest/ViewController.swift
+++ JenkinsTest/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIWebViewDelegate {
+    @IBOutlet weak var webview: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        webview.loadHTMLString("<script src=\"//player.cnevids.com/script/video/5aa81ea6841c4b32fa000009.js?autoplay=0\" class=\"x-skip\"></script>", baseURL: URL(string: "http:"))
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        print(error)
+    }
+    
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        
+        return true
     }
 
     override func didReceiveMemoryWarning() {
